4bc03d199a783b78be81a161e5f7626b189d887a
Abhishek De <abhishekde95@gmail.com>
2024-04-08 20:27:48 -0700

    more stuff

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0"
+DESCRIPTION = "Package for Projects"
+LONG_DESCRIPTION = "Package for Leetcode and Trading exercises"
+
+# Setting up
+setup(
+    name="utils",
+    version=VERSION,
+    author="Abhishek De",
+    author_email="abhishekde95@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "utils"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
