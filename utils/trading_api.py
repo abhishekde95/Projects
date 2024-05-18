1d4abc92d0c6bd3fa4725473ba7ba27e0388942e
Abhishek De <abhishekde95@gmail.com>
2024-05-18 15:01:13 -0700

    save

--- utils/trading_api.py
+++ utils/trading_api.py
@@ -1,3 +1,28 @@
-import numpy as np
-import pandas as pd
-import polars as ps
+import plotly.graph_objects as go
+import yfinance as yf
+
+
+def plot_ticker(ticker: yf.ticker.Ticker, mode: str = "png"):
+    mini_data = ticker.history(
+        period="1d",
+        interval="1m",
+        start=None,
+        end=None,
+        actions=True,
+        auto_adjust=True,
+        back_adjust=False,
+    )
+    fig = go.Figure(
+        data=go.Ohlc(
+            x=mini_data.index,
+            open=mini_data.Open,
+            high=mini_data.High,
+            low=mini_data.Low,
+            close=mini_data.Close,
+        )
+    )
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    if mode == "png":
+        fig.show("png")
+    else:
+        fig.show()
