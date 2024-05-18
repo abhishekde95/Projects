8af8be9bcec67723cf7f28ef92646b68a53c81c5
Abhishek De <abhishekde95@gmail.com>
2024-05-18 15:03:27 -0700

    revise

--- utils/trading_api.py
+++ utils/trading_api.py
@@ -2,16 +2,28 @@ import plotly.graph_objects as go
 import yfinance as yf
 
 
-def plot_ticker(ticker: yf.ticker.Ticker, mode: str = "png"):
+def plot_ticker(
+    ticker: yf.ticker.Ticker,
+    period: str = "1d",
+    interval: str = "1m",
+    mode: str = "png",
+):
     mini_data = ticker.history(
-        period="1d",
-        interval="1m",
+        period=period,
+        interval=interval,
         start=None,
         end=None,
         actions=True,
         auto_adjust=True,
         back_adjust=False,
     )
+
+    expected_columns = ["Open", "High", "Low", "Close"]
+    if not set(expected_columns).issubset(set(mini_data.columns)):
+        raise ValueError(
+            f"Expected columns: {expected_columns}, but got {mini_data.columns}"
+        )
+
     fig = go.Figure(
         data=go.Ohlc(
             x=mini_data.index,
